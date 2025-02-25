07375845399970f2609764c0eba6897187bb45da
MonySOEURN <soeurn.mony@gmail.com>
2025-02-25 16:02:22 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ rm -r mnist/all_data
 python generate_data.py \
   --dataset mnist \
   --n_clients 10 \
-  --iid \
+  --non_iid \
   --frac 0.2 \
   --save_dir mnist \
   --seed 1234
@@ -18,10 +18,9 @@ echo "=> Train.."
 
 python train.py \
   --experiment "mnist" \
-  --n_rounds 10 \
+  --n_rounds 20 \
   --local_steps 100 \
   --sampling_rate 0.2 \
-  --sample_with_replacement \
   --local_optimizer sgd \
   --local_lr 0.001 \
   --server_optimizer sgd \
